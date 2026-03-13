a858e88994844dc8a1590ff6d62b9270d3e8f615
theeasylearn <theeasylearn@gmail.com>
2026-03-13 18:22:32 +0530

    decision making statement 2

---
+++ 18_if_elif_1.py
@@ -0,0 +1,16 @@
+#write a program to findout whether how many days given month has 
+# 1 3 5 7 8 10 12 this month has 31 days 
+# 4 6 8 11  this month has 30 days 
+# 2 month has 28/29 days 
+
+month = int(input("Enter months "))
+if month<1 or month>12:
+    print("invalid month, month must be between range of 1 to 12")
+elif month == 2:
+    print("this month is 28/29 days")
+elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    print("this month has 31 days")
+else:
+    print("this month has 30 days")
+
+
