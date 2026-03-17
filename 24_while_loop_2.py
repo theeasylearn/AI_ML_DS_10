65fc98f38cc01ef74edb13f0dfc220080c9810b6
theeasylearn <theeasylearn@gmail.com>
2026-03-17 17:59:30 +0530

    while loop

--- 24_while_loop_2.py
+++ 24_while_loop_2.py
@@ -1,6 +1,15 @@
 # example of while loop 
 # 1 -4  9  -16 25 -36 49 -64 ..... 400 
 
+number = 1
+
+while number<=20:
+    square = number * number
+    if number%2==0: #1
+        square = 0 - square
+    print(square,end= ' ')
+    number = number + 1
+
 
 
 
--- 25_while_loop_3.py
+++ 25_while_loop_3.py
@@ -1,3 +1,14 @@
 #write a program to convert given amount into words 
 # input : 12345
 # output : one two three four five 
+
+words = ['zero ','one ','two ','three ','four ','five ','six ','seven ','eight ','nine ']
+list = []
+amount = 12345
+
+while amount>0:
+    reminder = amount % 10
+    list.insert(0,words[reminder]) # ['five']
+    amount = amount // 10
+
+print(''.join(list))
